a0e74f90311b9d09f77a6ad8bcec23ed0944ac07
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-21 20:01:34 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,9 +18,9 @@ def using_insert(array, element)
   array.insert(4, element)
 end
   
-# def using_concat(arr1, arr2)
-#   arr1 + arr2
-# end
+def using_concat(arr1, arr2)
+  arr1 + arr2
+end
 
 def shift_with_args(array)
   array.shift(2)
